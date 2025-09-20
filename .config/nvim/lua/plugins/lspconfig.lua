bac8e9c33da3269d33ca50505eb146218d1a321b
TheJltres <joseluistresserras@gmail.com>
2025-09-20 17:02:17 +0200

    Improve LSP and format options

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -15,21 +15,6 @@ return {
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
                 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
                 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-
-                local client = vim.lsp.get_client_by_id(args.data.client_id)
-                if not client then return end
-
-                if client.supports_method(client, "textDocument/formatting") then
-                    vim.api.nvim_create_autocmd('BufWritePre', {
-                        buffer = args.buf,
-                        callback = function()
-                            vim.lsp.buf.format({
-                                id = client.id,
-                                bufnr = args.buf,
-                            })
-                        end
-                    })
-                end
             end
         })
     end
